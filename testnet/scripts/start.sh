da6369c1e89e08c80fcab7ce3f43cf34e96e32c2
asherzyz <asher@dplabs.xyz>
2025-08-20 17:27:45 +0800

    delete alops

--- testnet/scripts/start.sh
+++ testnet/scripts/start.sh
@@ -17,19 +17,15 @@ if [ ! -d /data/pharos-node ]; then
     /root/.local/bin/pipenv run pharos --no-debug deploy /app/scripts/domain.json
     /root/.local/bin/pipenv run pharos --no-debug bootstrap /app/scripts/domain.json
     cd /data/pharos-node/domain/light/bin/
-    exec env LD_PRELOAD=./libevmone.so:./libdora_c.so ./pharos_light -c ../conf/launch.conf
+    exec env LD_PRELOAD=./libevmone.so ./pharos_light -c ../conf/launch.conf
 else
-    rm -f /data/pharos-node/domain/light/bin/libdora_c.so
     rm -f /data/pharos-node/domain/light/bin/libevmone.so
     rm -f /data/pharos-node/domain/light/bin/pharos_light
     rm -f /data/pharos-node/domain/light/bin/VERSION
     rm -f /data/pharos-node/domain/client/bin/pharos_cli
     rm -f /data/pharos-node/domain/client/bin/libevmone.so
-    rm -f /data/pharos-node/domain/client/bin/libdora_c.so
 
     ln -s /app/bin/libevmone.so /data/pharos-node/domain/client/bin/libevmone.so
-    ln -s /app/bin/libdora_c.so /data/pharos-node/domain/client/bin/libdora_c.so
-    ln -s /app/bin/libdora_c.so /data/pharos-node/domain/light/bin/libdora_c.so
     ln -s /app/bin/libevmone.so /data/pharos-node/domain/light/bin/libevmone.so
     ln -s /app/bin/pharos_light /data/pharos-node/domain/light/bin/pharos_light
     ln -s /app/bin/VERSION /data/pharos-node/domain/light/bin/VERSION
@@ -44,5 +40,5 @@ else
 
     ~/.local/bin/pipenv run pharos update-conf domain.json
     cd /data/pharos-node/domain/light/bin/
-    exec env LD_PRELOAD=./libevmone.so:./libdora_c.so ./pharos_light -c ../conf/launch.conf
+    exec env LD_PRELOAD=./libevmone.so ./pharos_light -c ../conf/launch.conf
 fi
