b969f3d475737f089e6d154c7649007b26a2ef0d
asherzyz <asher@dplabs.xyz>
2025-08-28 16:26:18 +0800

    update start.sh

--- testnet/scripts/start.sh
+++ testnet/scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 /usr/sbin/sshd
 
 external_ip=$(curl -s ifconfig.me || ip route get 1 | awk '{print $7; exit}')
@@ -9,10 +11,11 @@ if [ ! -d /data/pharos-node ]; then
 
     sed -i "s/\"host\": \"127.0.0.1\"/\"host\": \"$external_ip\"/" /app/scripts/deploy.light.json
     /root/.local/bin/pipenv run pharos --no-debug generate /app/scripts/deploy.light.json
+    /bin/cp -rf /app/scripts/resources /data
     sed -i 's|\"../conf/genesis.pharos-node.conf\"|\"/data/genesis.conf\"|g' /app/scripts/domain.json
-    cp domain.json /data/
-    cp deploy.light.json /data/
-    cp ../genesis.conf /data/genesis.conf
+    /bin/cp -f domain.json /data/
+    /bin/cp -f deploy.light.json /data/
+    /bin/cp -f ../genesis.conf /data/genesis.conf
 
     /root/.local/bin/pipenv run pharos --no-debug deploy /app/scripts/domain.json
     /root/.local/bin/pipenv run pharos --no-debug bootstrap /app/scripts/domain.json
@@ -31,12 +34,13 @@ else
     ln -s /app/bin/VERSION /data/pharos-node/domain/light/bin/VERSION
 
     cd /app/scripts
-    cp /data/domain.json .
-    cp -rf /data/pharos-node/domain/light/conf/monitor.conf /app/conf/monitor.conf
-    cp -rf /data/pharos-node/domain/client/conf/genesis.conf /app/conf/
+    /bin/cp -f /data/domain.json .
+    /bin/cp -rf /data/resources /app/scripts
+    /bin/cp -f /data/pharos-node/domain/light/conf/monitor.conf /app/conf/monitor.conf
+    /bin/cp -f /data/pharos-node/domain/client/conf/genesis.conf /app/conf/
 
-    cp -rf /app/bin/pharos_cli /data/pharos-node/domain/client/bin/
-    cp -rf /data/deploy.light.json .
+    /bin/cp -f /app/bin/pharos_cli /data/pharos-node/domain/client/bin/
+    /bin/cp -f /data/deploy.light.json .
 
     ~/.local/bin/pipenv run pharos update-conf domain.json
     cd /data/pharos-node/domain/light/bin/
