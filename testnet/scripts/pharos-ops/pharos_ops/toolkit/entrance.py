1b24a629d31fe365526965ed08e18b0206f51b9e
akiozihao <akiozihaoceat@gmail.com>
2025-09-23 20:35:53 +0800

    delete unused

--- testnet/scripts/pharos-ops/pharos_ops/toolkit/entrance.py
+++ testnet/scripts/pharos-ops/pharos_ops/toolkit/entrance.py
@@ -294,12 +294,11 @@ def upgrade():
     deploy_data = json.load(fh)
     deploy = DeploySchema().load(deploy_data)
 
-    deploy_path = f"{deploy.deploy_root}/"
+    deploy_path = f"{deploy.deploy_root}"
     original_cwd = os.getcwd()
     logs.info(f"original_cwd: {original_cwd}")
     logs.info(f"deploy_path: {deploy_path}")
     files_to_remove = [
-        f"{deploy_path}/domain/light/bin/libdora_c.so",
         f"{deploy_path}/domain/light/bin/libevmone.so",
         f"{deploy_path}/domain/light/bin/pharos_light",
         f"{deploy_path}/domain/light/bin/VERSION",
@@ -310,10 +309,6 @@ def upgrade():
             os.remove(file_path)
 
     symlinks = [
-        (
-            f"{original_cwd}/../bin/libdora_c.so",
-            f"{deploy_path}/domain/light/bin/libdora_c.so",
-        ),
         (
             f"{original_cwd}/../bin/libevmone.so",
             f"{deploy_path}/domain/light/bin/libevmone.so",
@@ -333,27 +328,14 @@ def upgrade():
         os.symlink(src, dst)
 
     try:
-        shutil.copyfile(
-            f"{deploy_path}/domain/light/conf/monitor.conf",
-            f"{original_cwd}/../conf/monitor.conf",
-        )
-
-        local.run(
-            "~/.local/bin/pipenv run pharos update-conf domain.json",
-        )
+        # shutil.copyfile(
+        #     f"{deploy_path}/domain/light/conf/monitor.conf",
+        #     f"{original_cwd}/../conf/monitor.conf",
+        # )
 
         shutil.copyfile(
             f"{original_cwd}/../bin/pharos_cli",
             f"{deploy_path}/domain/client/bin/pharos_cli",
-        )
-
-        os.chdir("/data/pharos-node/domain/light/bin/")
-        current_env = os.environ.copy()
-        current_env["LD_PRELOAD"] = "./libevmone.so:./libdora_c.so"
-        logs.info("x")
-        subprocess.run(
-            ["./pharos_light", "-c", "../conf/launch.conf"],
-            env=current_env,
         )
     except Exception as e:
         logs.error(f"Upgrade failed: {e}")
